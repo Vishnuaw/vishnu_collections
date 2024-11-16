ca27a7c706a8e7e6af79b50a1ce715481cbbf550
Vishnuaw <vishnuvardhanvfx@gmail.com>
2024-11-16 11:48:42 +0530

    vishnu resources

---
+++ Javascript/script.js
@@ -0,0 +1,7 @@
+const name = document.getElementById('name')
+const pass = document.getElementById('password')
+const form = document.getElementById('form') 
+
+form.addEventListener('submit',() => {
+ e.preventDefault    
+}   )
